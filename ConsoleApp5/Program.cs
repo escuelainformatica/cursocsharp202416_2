1eb39c973c7e40c4b51cb6a9386432331287e547
jorgecc <jcastro@eftec.cl>
2024-10-16 16:59:38 -0300

    primer

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp5.modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // primer ejercicio
+            var mountain = new Bicicleta();
+            mountain.nombre = "marca1";
+            mountain.marca = "Oxford";
+            mountain.velocidades = 10;
+
+            Console.WriteLine(mountain.nombre);
+            Console.WriteLine(mountain.marca);
+            Console.WriteLine(mountain.velocidades);
+
+            // ejercicio 2
+            var fac1 = new Factura
+            {
+                numFactura = 1,
+                nombreCliente = "vtr",
+                fecha = "01/01/2010",
+                monto = 434343
+            };
+            // ejercicio 3
+            var peugeot = new Auto
+            {
+                patente="XXBB-33",
+                marca="Peugeot",
+
+            };
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp5/modelo/Bicicleta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.modelo
+{
+    internal class Bicicleta
+    {
+        public string nombre;
+        public int velocidades;
+        public string marca;
+    }
+}
